1138ac44f27935c953896dc50e66182ce4d46d4a
Michael Gutterman <programmer94@users.noreply.github.com>
2024-02-26 20:13:19 -0500

    Remove unused import statements

--- src/main/java/frc/robot/subsystems/PracticeSwerveHw.java
+++ src/main/java/frc/robot/subsystems/PracticeSwerveHw.java
@@ -12,7 +12,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 
 @SuppressWarnings("removal")
@@ -231,7 +230,7 @@ public class PracticeSwerveHw implements ISwerveDriveIo {
 
     @Override
     public void updateInputs() {
-
+        // No op
     }
 
 }
